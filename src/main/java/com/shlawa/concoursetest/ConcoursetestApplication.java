f8cf3629167162cfc1fb5f8cfcace61362906db8
Sebastian H <s.hlawa@meteogroup.com>
2016-06-14 17:45:22 +0200

    I forgot maven stuff...

---
+++ src/main/java/com/shlawa/concoursetest/ConcoursetestApplication.java
@@ -0,0 +1,12 @@
+package com.shlawa.concoursetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcoursetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcoursetestApplication.class, args);
+	}
+}
